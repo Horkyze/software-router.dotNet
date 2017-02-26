f30e66c54d3e273e43a5b8fefb90d6ad16e30091
Matej <matej.bellus@gmail.com>
2017-02-26 12:34:27 +0100

    working device listing

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PcapDotNet.Core;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,36 @@ namespace sw_router
         public Form1()
         {
             InitializeComponent();
+            PortListner p = new PortListner();
+            p.getInterfacesList();
+            dataGridView1.DataSource = p.allDevices;
+            dataGridView1.Refresh();
+            foreach (LivePacketDevice d in p.allDevices)
+            {
+                richTextBox1.AppendText(d.Name + "\n");
+                richTextBox1.AppendText(d.Addresses + "\n");
+                richTextBox1.AppendText(d.Description + "\n");
+                richTextBox1.AppendText(d.Attributes + "\n");
+                comboBox1.Items.Add(d.Description);
+                comboBox2.Items.Add(d.Description);
+
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ portListner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PcapDotNet.Base;
+using PcapDotNet.Core;
+
+
+namespace sw_router
+{
+
+    class PortListner
+    {
+        public IList<LivePacketDevice> allDevices;
+        public void getInterfacesList() {
+            // Retrieve the device list from the local machine
+            allDevices = LivePacketDevice.AllLocalMachine;
+        
+
+            if (allDevices.Count == 0)
+            {
+                Console.WriteLine("No interfaces found! Make sure WinPcap is installed.");
+                return;
+            }
+
+            // Print the list
+            for (int i = 0; i != allDevices.Count; ++i)
+            {
+                LivePacketDevice device = allDevices[i];
+            Console.Write((i + 1) + ". " + device.Name);
+                if (device.Description != null)
+                    Console.WriteLine(" (" + device.Description + ")");
+                else
+                    Console.WriteLine(" (No description available)");
+            }
+        }
+    }
+}
